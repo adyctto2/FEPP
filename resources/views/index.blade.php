1b98898dbdb0fd02cb76788c14da0591a8122b04
Guido Lopez Meriles <adyctto@gmail.com>
2018-09-26 22:54:51 -0400

    FEPP

---
+++ resources/views/index.blade.php
@@ -0,0 +1,624 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Federacion de Empresarios Privados Potosí</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+	<link type="text/css" rel="stylesheet" href="css/owl.carousel.css" />
+	<link type="text/css" rel="stylesheet" href="css/owl.theme.default.css" />
+	<link type="text/css" rel="stylesheet" href="css/magnific-popup.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+	<header id="home">
+		<div class="bg-img" style="background-image: url('./img/background1.jpg');">
+			<div class="overlay"></div>
+		</div>
+		<!-- /Background Image -->
+
+		<!-- Nav -->
+		<nav id="nav" class="navbar nav-transparent">
+			<div class="container">
+
+				<div class="navbar-header">
+					<!-- Logo -->
+					<div class="navbar-brand">
+						<a href="#home">
+							<img class="logo" src="img/logo.png" alt="logo">
+							<img class="logo-alt" src="img/logo.png" alt="logo">
+						</a>
+					</div>
+					<!-- /Logo -->
+
+					<!-- Collapse nav button -->
+					<div class="nav-collapse">
+						<span></span>
+					</div>
+					<!-- /Collapse nav button -->
+				</div>
+
+				<!--  Main navigation  -->
+				<ul class="main-nav nav navbar-nav navbar-right">
+					<li><a href="#home">Inicio</a></li>
+                    <li><a href="#about">La F.E.P.P.</a></li>
+					<li><a href="#portfolio">Entidades Afiliadas</a></li>
+					<li><a href="#service">Eventos</a></li>
+					<li><a href="#team">Noticias</a></li>
+					<li><a href="#">FEIPOBOL 2018</a></li>
+					<li><a href="#contacto">Contacto</a></li>
+				</ul>
+				<!-- /Main navigation -->
+
+			</div>
+		</nav>
+		<!-- /Nav -->
+
+		<!-- home wrapper -->
+		<div class="home-wrapper">
+			<div class="container">
+				<div class="row">
+
+					<!-- home content -->
+					<div class="col-md-10 col-md-offset-1">
+						<div class="home-content">
+							<img class="img-responsive" src="./img/logoPrincipal.png" alt="">
+							<a href="#about"><button type="button" name="button" class = "main-btn btn-danger">INGRESAR</button></a>
+						</div>
+					</div>
+					<!-- /home content -->
+
+				</div>
+			</div>
+		</div>
+		<!-- /home wrapper -->
+
+	</header>
+	<!-- /Header -->
+
+	<!-- About -->
+	<div id="about" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section header -->
+				<div class="section-header text-center">
+					<img class="img-responsive img-center" src="./img/logoFepp.png" alt="">
+					<h1>FEDERACIÓN DE EMPRESARIOS PRIVADOS POTOSÍ</h1>
+					<h3>La  FEPP  se  constituye  en  una  institución  que  no  desarrolla  actividades  político partidarias, con un Comité Ejecutivo renovado con el único objetivo de trabajar de manera transparente en beneficio del sector privado, compartiendo sus conocimientos y habilidades necesarias para promover eficazmente la creatividad y la innovación empresarial</h3>
+				</div>
+				<!-- /Section header -->
+
+				<!-- about -->
+				<div class="col-md-4">
+					<div class="about">
+						<h3>MISIÓN</h3>
+						<p>Ser una entidad que concentre al sector empresarial privado, promoviendo el fortalecimiento y desarrollo institucional, velando los intereses jurídicos, económicos, sociales de nuestros afiliados, generando participación activa en el progreso departamental y nacional.</p>
+						<a href="#">Leer mas</a>
+					</div>
+				</div>
+				<!-- /about -->
+
+				<!-- about -->
+				<div class="col-md-4">
+					<div class="about">
+						<h3>VISIÓN</h3>
+						<p>Ser una entidad líder representativa e influyente, prestigiosa y solvente, reconocida por su contribución al desarrollo productivo, económico social y turístico del Departamento de Potosí.</p>
+						<a href="#">Leer mas</a>
+					</div>
+				</div>
+				<!-- /about -->
+
+				<!-- about -->
+				<div class="col-md-4">
+					<div class="about">
+						<h3>OBJETIVO</h3>
+						<p>Promover la participación del sector empresarial en aspectos de interés local y regional, que coadyuve al desarrollo productivo, económico social y turístico del departamento, sustentando la filosofía de la libre empresa y la economía del mercado, como bases esenciales de dicho proceso.</p>
+						<a href="#">Leer mas</a>
+					</div>
+				</div>
+				<!-- /about -->
+
+			</div>
+			
+		</div>
+		<!-- /Container -->
+		<div id="testimonial" class="section md-padding">
+
+			<!-- Background Image -->
+			<div class="bg-img" style="background-image: url('./img/background3.jpg');">
+				<div class="overlay"></div>
+			</div>
+			<!-- /Background Image -->
+	
+			<!-- Container -->
+			<div class="container">
+	
+				<!-- Row -->
+				<div class="row">
+	
+					<!-- Testimonial slider -->
+					<div class="col-md-10 col-md-offset-1">
+						<div id="testimonial-slider" class="owl-carousel owl-theme">
+	
+							<!-- testimonial -->
+							<div class="testimonial">
+								<div class="testimonial-meta">
+									<img src="./img/perso1.jpg" alt="">
+									<h3 class="white-text">Juan Perez</h3>
+									<span>PRESIDENTE COMITE EJECUTIVO</span>
+								</div>
+								<p class="white-text">Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar</p>
+							</div>
+							<div class="testimonial">
+								<div class="testimonial-meta">
+									<img src="./img/perso1.jpg" alt="">
+									<h3 class="white-text">Pedro Lopez</h3>
+									<span>PRESIDENTE COMITE EJECUTIVO</span>
+								</div>
+								<p class="white-text">Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar</p>
+							</div>
+							<div class="testimonial">
+								<div class="testimonial-meta">
+									<img src="./img/perso1.jpg" alt="">
+									<h3 class="white-text">Ramiro Ramirez</h3>
+									<span>PRESIDENTE COMITE EJECUTIVO</span>
+								</div>
+								<p class="white-text">Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar</p>
+							</div>
+							<div class="testimonial">
+								<div class="testimonial-meta">
+									<img src="./img/perso1.jpg" alt="">
+									<h3 class="white-text">Camila Caballero</h3>
+									<span>PRESIDENTE COMITE EJECUTIVO</span>
+								</div>
+								<p class="white-text">Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar</p>
+							</div>
+							<div class="testimonial">
+								<div class="testimonial-meta">
+									<img src="./img/perso1.jpg" alt="">
+									<h3 class="white-text">Maria Martinez</h3>
+									<span>PRESIDENTE COMITE EJECUTIVO</span>
+								</div>
+								<p class="white-text">Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar</p>
+							</div>
+							<!-- /testimonial -->
+	
+						</div>
+					</div>
+					<!-- /Testimonial slider -->
+	
+				</div>
+				<!-- /Row -->
+	
+			</div>
+			<!-- /Container -->
+	
+		</div>
+	</div>
+	<!-- /About -->
+	<div class="bg-img" style="background-image: url('./img/background3.jpg');">
+		<div class="overlay"></div>
+	</div>
+
+	<!-- Portfolio -->
+	<div id="portfolio" class="section md-padding bg-grey">
+		
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section header -->
+				<div class="section-header text-center">
+					<h2 class="title">Entidades Afiliadas</h2>
+				</div>
+				<!-- /Section header -->
+
+				<!-- Work -->
+				<div class="col-md-3 col-xs-4 work">
+					<img class="img-responsive" src="./img/work1.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>INFOCAL POTOSI</h3>
+						<div class="work-link">
+							<a href="#"><i class="fa fa-external-link"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+
+				<!-- Work -->
+				<div class="col-md-3 col-xs-4 work">
+					<img class="img-responsive" src="./img/work2.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>CADECO POTOSI</h3>
+						<div class="work-link">
+							<a href="#"><i class="fa fa-external-link"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+			
+				<div class="col-md-3 col-xs-4 work">
+					<img class="img-responsive" src="./img/work2.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>CADECO POTOSI</h3>
+						<div class="work-link">
+							<a href="#"><i class="fa fa-external-link"></i></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-xs-4 work">
+					<img class="img-responsive" src="./img/work1.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>INFOCAL POTOSI</h3>
+						<div class="work-link">
+							<a href="#"><i class="fa fa-external-link"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+
+				<!-- Work -->
+				<div class="col-md-3 col-xs-4 work">
+					<img class="img-responsive" src="./img/work2.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>CADECO POTOSI</h3>
+						<div class="work-link">
+							<a href="#"><i class="fa fa-external-link"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+			
+				<div class="col-md-3 col-xs-4 work">
+					<img class="img-responsive" src="./img/work2.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>CADECO POTOSI</h3>
+						<div class="work-link">
+							<a href="#"><i class="fa fa-external-link"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Portfolio -->
+	<div id="numbers" class="section sm-padding">
+
+		<!-- Background Image -->
+		<div class="bg-img" style="background-image: url('./img/background2.jpg');">
+			<div class="overlay"></div>
+		</div>
+		<!-- /Background Image -->
+
+		<!-- Container -->
+		<div class="container">
+
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- Service -->
+	<div id="service" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section header -->
+				<div class="section-header text-center">
+					<h2 class="title">Eventos</h2>
+				</div>
+				<!-- /Section header -->
+
+				<!-- service -->
+				<div class="col-md-4">
+					<div class="blog">
+						<div class="blog-img">
+							<img class="img-responsive" src="./img/blog1.jpg" alt="">
+						</div>
+						<div class="blog-content">
+							<ul class="blog-meta">
+								<li><i class="fa fa-clock-o"></i>18 Octubre de 2018</li>
+							</ul>
+							<h3>evento 1 prueba</h3>
+							<p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+							<a href="blog-single.html">Leer Completo</a>
+						</div>
+					</div>
+				</div>
+				<!-- /blog -->
+
+				<!-- blog -->
+				<div class="col-md-4">
+					<div class="blog">
+						<div class="blog-img">
+							<img class="img-responsive" src="./img/blog2.jpg" alt="">
+						</div>
+						<div class="blog-content">
+							<ul class="blog-meta">
+								<li><i class="fa fa-clock-o"></i>18 Octubre de 2018</li>
+							</ul>
+							<h3>evento 2 prueba</h3>
+							<p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+							<a href="blog-single.html">Leer Completo</a>
+						</div>
+					</div>
+				</div>
+				<!-- /blog -->
+
+				<!-- blog -->
+				<div class="col-md-4">
+					<div class="blog">
+						<div class="blog-img">
+							<img class="img-responsive"  src="./img/blog3.jpg" alt="">
+						</div>
+						<div class="blog-content">
+							<ul class="blog-meta">
+								<li><i class="fa fa-clock-o"></i>18 Octubre de 2018</li>
+							</ul>
+							<h3>Evento 3 prueba</h3>
+							<p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+							<a href="blog-single.html">Leer Completo</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Service -->
+
+	<div id="numbers" class="section sm-padding">
+
+		<!-- Background Image -->
+		<div class="bg-img" style="background-image: url('./img/background2.jpg');">
+			<div class="overlay"></div>
+		</div>
+		<!-- /Background Image -->
+
+		<!-- Container -->
+		<div class="container">
+
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+
+	<!-- Team -->
+	<div id="team" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section header -->
+				<div class="section-header text-center">
+					<h2 class="title">Noticias</h2>
+				</div>
+				<!-- service -->
+				<div class="col-md-4">
+					<div class="blog">
+						<div class="blog-img">
+							<img class="img-responsive" src="./img/blog1.jpg" alt="">
+						</div>
+						<div class="blog-content">
+							<ul class="blog-meta">
+								<li><i class="fa fa-clock-o"></i>18 Octubre de 2018</li>
+							</ul>
+							<h3>Noticia 1 prueba</h3>
+							<p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+							<a href="blog-single.html">Leer Completo</a>
+						</div>
+					</div>
+				</div>
+				<!-- /blog -->
+
+				<!-- blog -->
+				<div class="col-md-4">
+					<div class="blog">
+						<div class="blog-img">
+							<img class="img-responsive" src="./img/blog2.jpg" alt="">
+						</div>
+						<div class="blog-content">
+							<ul class="blog-meta">
+								<li><i class="fa fa-clock-o"></i>18 Octubre de 2018</li>
+							</ul>
+							<h3>Noticia 2 prueba</h3>
+							<p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+							<a href="blog-single.html">Leer Completo</a>
+						</div>
+					</div>
+				</div>
+				<!-- /blog -->
+
+				<!-- blog -->
+				<div class="col-md-4">
+					<div class="blog">
+						<div class="blog-img">
+							<img class="img-responsive"  src="./img/blog3.jpg" alt="">
+						</div>
+						<div class="blog-content">
+							<ul class="blog-meta">
+								<li><i class="fa fa-clock-o"></i>18 Octubre de 2018</li>
+							</ul>
+							<h3>Noticia 3 prueba</h3>
+							<p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+							<a href="blog-single.html">Leer Completo</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Team -->
+
+
+	<!-- Numbers -->
+	<div id="numbers" class="section sm-padding">
+
+		<!-- Background Image -->
+		<div class="bg-img" style="background-image: url('./img/background2.jpg');">
+			<div class="overlay"></div>
+		</div>
+		<!-- /Background Image -->
+
+		<!-- Container -->
+		<div class="container">
+
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Numbers -->
+	<!-- Contact -->
+	<div id="contacto" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section-header -->
+				<div class="section-header text-center">
+					<h2 class="title">Contactanos</h2>
+				</div>
+				<div class="col-sm-4">
+					<div class="contact">
+						<i class="fa fa-phone"></i>
+						<h3>Telefono</h3>
+						<p>62-24565</p>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="contact">
+						<i class="fa fa-envelope"></i>
+						<h3>Correo Electronico</h3>
+						<p>fepp@gmail.com</p>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="contact">
+						<i class="fa fa-map-marker"></i>
+						<h3>Direccion</h3>
+						<p>Potosí -  Bolivia</p>
+					</div>
+				</div>
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Contact -->
+
+
+	<!-- Footer -->
+	<footer id="footer" class="sm-padding bg-dark">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<div class="col-md-12">
+
+					<!-- footer logo -->
+					<div class="footer-logo">
+						<a href="index.html"><img src="img/logo-alt.png" alt="logo"></a>
+					</div>
+					<!-- /footer logo -->
+
+					<!-- footer follow -->
+					<ul class="footer-follow">
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+					</ul>
+					<!-- /footer follow -->
+
+					<!-- footer copyright -->
+					<div class="footer-copyright">
+						<p>Copyright © 2018. Todos los derechos reservados F.E.P.P. diseñado por <a href="https://adyctto.com" target="_blank">I S & T</a></p>
+					</div>
+					<!-- /footer copyright -->
+
+				</div>
+
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</footer>
+	<!-- /Footer -->
+
+	<!-- Back to top -->
+	<div id="back-to-top"></div>
+	<!-- /Back to top -->
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<div class="preloader">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+	<!-- /Preloader -->
+
+	<!-- jQuery Plugins -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+
+</body>
+
+</html>
