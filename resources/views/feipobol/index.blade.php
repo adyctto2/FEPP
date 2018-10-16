c1bca219154b3c2959be63c51168204a61012fcf
Guido Lopez Meriles <adyctto@gmail.com>
2018-10-16 13:44:52 -0400

    algo

--- resources/views/feipobol/index.blade.php
+++ resources/views/feipobol/index.blade.php
@@ -11,7 +11,7 @@
   <!-- Bootstrap core CSS -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
   <!-- Material Design Bootstrap -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.12/css/mdb.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.11/css/mdb.min.css" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('css/styleFeipobol.css')}}">
 </head>
 <body>
@@ -20,7 +20,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark color fixed-top scrolling-navbar">
     <div class="container">
       <a class="navbar-brand" href="#">
-        <img src="{{ asset('img/logos/feipobol.png')}}" height="50" alt="mdb logo" class="d-inline-block align-top" alt="FEIPOBOL 2018">
+        <img src="{{ asset('img/logos/feipobol.png')}}" height="35" alt="mdb logo" class="d-inline-block align-top" alt="FEIPOBOL 2018">
       </a>
       <!-- Collapse button -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
@@ -34,48 +34,131 @@
       <!-- Links -->
       <ul class=" navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home
+          <a class="nav-link" href="#">Inicio
             <span class="sr-only">(current)</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Acerca de</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Noticias</a>
         </li>
         
         <!-- Dropdown -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
-          aria-expanded="false">Dropdown</a>
+          aria-expanded="false">Servicios</a>
           <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
+            <a class="dropdown-item" href="#">Venta de Stands</a>
           </div>
         </li>
         
+        <li class="nav-item">
+            <a class="nav-link" href="#">Galeria</a>
+          </li>
+          
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contactos</a>
+          </li>
+          
       </ul>
     </div>
   </div>
   <!-- Collapsible content -->  
 </nav>
+<div class="view" style="">
+  <!-- Mask & flexbox options-->
+  <div class="mask rgba-gradient d-flex justify-content-center align-items-center">
+    <!-- Content -->
+    <div class="container">
+      <!--Grid row-->
+      <div class="row mt-5">
+        <!--Grid column-->
+        <div class="col-md-6 mb-5 mt-md-0 mt-5 white-text text-center text-md-left">
+          <h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">FEIPOBOL 2018</h1>
+          <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
+          <h6 class="mb-3 wow fadeInLeft" data-wow-delay="0.3s">La FEIPOBOL, uno de los eventos multisectoriales de negocios más importantes de Bolivia, bajo el modelo de internacionalización del negocio, se ha consolidado como protagonista activa para el intercambio comercial y la movilización del desarrollo productivo, comercial y de servicios a nivel local, nacional e internacional.</h6>
+          <a class="btn btn-outline-white wow fadeInLeft" data-wow-delay="0.3s">Saber mas</a>
+        </div>
+        <!--Grid column-->
+        <!--Grid column-->
+        <div class="col-md-6 col-xl-5 mb-4">
+          <!--Form-->
+          <div class="card wow fadeInRight" data-wow-delay="0.3s">
+            <div class="text-center">
+              <h3 class="white-text">
+                <img src="{{ asset('img/logos/feipobol.png')}}" class="logo" alt="" width="60%">
+            </div>
+            <div class="card-body">
+              <!--Body-->
+              <div class="md-form">
+                <i class="fa fa-calendar prefix white-text active"></i>
+                <h4>9 al 18 de noviembre</h4>
+              </div>
+              <div class="md-form">
+                  <a href=""><i class="fa fa-map-marker prefix white-text active"></i>
+                <h4>Campo Ferial Multipropósito (Av. Sevilla)
+                    Ciudad Potosi, Potosi, Bolivia</h4></a>
+              </div>
+              <div class="text-center mt-4">
+                <hr class="hr-light mb-3 mt-4">
+                <div class="inline-ul text-center d-flex justify-content-center">
+                  <a class="p-2 m-2 tw-ic">
+                    <i class="fa fa-twitter white-text"></i>
+                  </a>
+                  <a class="p-2 m-2 li-ic">
+                    <i class="fa fa-facebook white-text"> </i>
+                  </a>
+                  <a class="p-2 m-2 ins-ic">
+                    <i class="fa fa-instagram white-text"> </i>
+                  </a>
+                  <a class="p-2 m-2 ins-ic">
+                    <i class="fa fa-youtube white-text"> </i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!--/.Form-->
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Content -->
+  </div>
+  <!-- Mask & flexbox options-->
+</div>
 </header>
 <!--/.Navbar-->
 
-<section class="portada">
-    <video autoplay>
-      <source src="{{asset('videos/intro.mp4')}}" type="video/mp4">
-    </video>
-  </section>
-  
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
   <!-- Bootstrap core JavaScript -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.12/js/mdb.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.11/js/mdb.min.js"></script>
 </body>
 </html>
